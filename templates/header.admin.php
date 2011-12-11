0b84e24a8b2f4d7d805175840a7314ac3d375d14
waisir <waisir@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-11 13:19:49 +0000

    git-svn-id: https://plugins.svn.wordpress.org/1000eb/trunk@473459 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/header.admin.php
+++ templates/header.admin.php
@@ -1,2 +1,2 @@
-<link href="<?php echo plugins_url();?>/wp-3721up/styles/admin.css" type="text/css" rel="stylesheet" />
-<script type="text/javascript" src="<?php echo plugins_url();?>/wp-3721up/javascripts/admin.js"></script>
+<link href="<?php echo plugins_url();?>/1000eb/styles/admin.css" type="text/css" rel="stylesheet" />
+<script type="text/javascript" src="<?php echo plugins_url();?>/1000eb/javascripts/admin.js"></script>
